8a014ee30a7a4eab341082a7e8ad25138edc8776
Neil Martey-Botchway <neil.marteybotchway@gmail.com>
2025-02-22 19:31:06 +0000

    Fix modals that were buggy

--- drink_less/lib/games/shape_rotation.dart
+++ drink_less/lib/games/shape_rotation.dart
@@ -1,7 +1,6 @@
 import 'package:drink_less/pages/start_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tuple/tuple.dart';
 
 class ShapeRotation extends StatefulWidget {
   final int questionNumber;
@@ -21,11 +20,7 @@ class SAR {
       : question = Image.asset("assets/images/q$qn/Q.png"),
         answer = Image.asset("assets/images/q$qn/Answer.png"),
         options =
-        [
-          "B",
-          "C",
-          "D",
-        ].map((n) => Image.asset("assets/images/q$qn/$n.png")).toList();
+        ["B", "C", "D"].map((n) => Image.asset("assets/images/q$qn/$n.png")).toList();
 
   List<(Image, String)> getAnswers() {
     var lst = List<Image>.from(options);
@@ -36,18 +31,61 @@ class SAR {
 }
 
 class _ShapeRotationState extends State<ShapeRotation> {
+  bool gameStarted = false; // Ensures game starts only after modal dismissal
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Show the modal only for the first question
+    if (widget.questionNumber == 1) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _showStartDialog();
+      });
+    } else {
+      gameStarted = true; // Automatically start the game for subsequent questions
+    }
+  }
+
+  void _showStartDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false, // Prevents accidental dismissal
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Welcome to Shape Rotation!'),
+          content: Text(
+            'Choose the correct rotated shape to advance to the next question.',
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+                setState(() {
+                  gameStarted = true; // Game starts after modal is dismissed
+                });
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final sar = SAR(widget.questionNumber);
+
     return Scaffold(
-      appBar: AppBar(title: Text("Shapes and rotation")),
-      body: Column(
+      appBar: AppBar(title: Text("Shapes and Rotation")),
+      body: gameStarted
+          ? Column(
         children: [
           Padding(padding: EdgeInsets.all(20), child: sar.question),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children:
-            sar
+            children: sar
                 .getAnswers()
                 .map(
                   (c) => Expanded(
@@ -60,14 +98,8 @@ class _ShapeRotationState extends State<ShapeRotation> {
                           if (c.$1 == sar.answer) print("RIGHT ANSWER");
                           Navigator.of(context).push(
                             MaterialPageRoute(
-                              builder:
-                                  (context) =>
-                              widget.questionNumber < 5
-                                  ? ShapeRotation(
-                                questionNumber:
-                                widget.questionNumber +
-                                    1,
-                              )
+                              builder: (context) => widget.questionNumber < 5
+                                  ? ShapeRotation(questionNumber: widget.questionNumber + 1)
                                   : StartTestPage(),
                             ),
                           );
@@ -83,6 +115,9 @@ class _ShapeRotationState extends State<ShapeRotation> {
                 .toList(),
           ),
         ],
+      )
+          : Center(
+        child: CircularProgressIndicator(), // Shows a loader until the modal is dismissed
       ),
     );
   }
