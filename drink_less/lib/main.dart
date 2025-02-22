cc57e0194d98261d940170fc6b90706924e28a2c
Neil Martey-Botchway <neil.marteybotchway@gmail.com>
2025-02-22 19:37:38 +0000

    fix more bugs

--- drink_less/lib/main.dart
+++ drink_less/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:drink_less/games/memory_match_game.dart';
 import 'package:drink_less/pages/start_page.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: StartTestPage(),
+      home: MemoryMatchGame(),
     );
   }
 }
