e273ffa3417133d33c40c0a1ad1871bd270db0aa
Isaac <59113736+toluwalase104@users.noreply.github.com>
2025-02-22 19:09:10 +0000

    Joseph found out the fix was to unwrap the message variable before putting it in response

--- drink_less/lib/net/face_scan.dart
+++ drink_less/lib/net/face_scan.dart
@@ -18,17 +18,6 @@ Future<void> getFaceScan(String assetPath) async {
 
       debugPrint('Decoded Encoding : ${base64Decode(message)}\n');
 
-      // String base64Image = "";
-
-      // if (buffer.isNotEmpty){
-      //   base64Image = base64Encode(imageList);
-      // } else {
-      //   debugPrint("No image list found");
-      //   throw UnimplementedError();
-      // }
-
-      // debugPrint('$base64Image\n');
-
       debugPrint(message);
 
       final model = 'drunk-or-sober-iwnwj/1';
@@ -40,20 +29,11 @@ Future<void> getFaceScan(String assetPath) async {
 
       final headers = 
           {'Content-Type': 'application/x-www-form-urlencoded'};
-          // {'Content-Type': 'application/json'};
-
-      // final body = {
-      //   "data": message,
-      // };
-      final body = {'image' : message};
-
-      // final encodedBody = Uri(queryParameters: body).query;
 
       final response = await http.post(
         Uri.parse(url),
         headers: headers,
-        // body: encodedBody,
-        body :  body,
+        body :  message,
       );
 
       if (response.statusCode == 200) {
@@ -67,78 +47,4 @@ Future<void> getFaceScan(String assetPath) async {
       debugPrint('Exception: ${e.toString()}');
     }
 }
-
-/*
-Future<void> getFaceScan(File imagePath) async {
-    try {
-      final bytes = imagePath.readAsBytesSync();
-      final imageList = bytes.buffer.asUint8List();
-
-      String base64Image = "";
-
-      if (imageList.isNotEmpty){
-        base64Image = base64Encode(imageList);
-      } else {
-        debugPrint("No image list found");
-        throw UnimplementedError();
-      }
-
-      final model = 'drunk-or-sober-iwnwj/1';
-
-      final apiKey = '22tzVfDkBC7oIorFHU2Y';
-
-      final url =
-          'https://classify.roboflow.com/$model?api_key=$apiKey';
-      final headers = {'Content-Type': 'application/x-www-form-urlencoded'};
-
-      final body = {
-        "data": base64Image,
-      };
-
-      final encodedBody = Uri(queryParameters: body).query;
-      final response = await http.post(
-        Uri.parse(url),
-        headers: headers,
-        body: encodedBody,
-      );
-
-      if (response.statusCode == 200) {
-        debugPrint('Success: ${response.body}');
-      } else {
-        debugPrint(
-            'Error: Status Code ${response.statusCode}, Response: ${response.body}');
-      }
-    } catch (e) {
-      debugPrint('Exception: ${e.toString()}');
-    }
-}
-*/
-
-/*
-  Initial attempt using internal processes within Flutter.
-  Runs into issues as it requires a python binary to be installed
-  within the application.
-  Could potentially be retried though.
-
-performScan(String imagePath) async {
-  try {
-    debugdebugPrint('Starting scan...');
-    var result = 
-      await Process.run(
-              'python',
-              [
-                'lib/net/python_code/detect_drunk.py', 
-                imagePath,
-              ]
-            );
-
-    debugdebugPrint('Scan Result - ${result.stdout}');
-    debugdebugPrint('Scan completed.');
-  } catch (e) {
-    debugdebugPrint("An error occurred:");
-    debugdebugPrint('$e');
-  }
-}
-
-*/
 
