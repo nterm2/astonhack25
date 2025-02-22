f13cf9b21f8cc8235fc976c4c8c81572f681763f
Joseph Glynn <josephnglynn@hotmail.co.uk>
2025-02-22 19:27:34 +0000

    cleanup

--- drink_less/lib/main.dart
+++ drink_less/lib/main.dart
@@ -1,12 +1,4 @@
-import 'package:drink_less/games/clicking.dart';
-import 'package:drink_less/games/memory_match_game.dart';
-import 'package:drink_less/games/shape_rotation.dart';
-import 'package:drink_less/games/tree.dart';
-import 'package:drink_less/pages/clicking_page.dart';
-import 'package:drink_less/pages/picture_page.dart';
 import 'package:drink_less/pages/start_page.dart';
-import 'package:flutter/foundation.dart';
-import 'package:drink_less/results/results.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +7,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
