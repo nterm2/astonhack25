07bac83d046c715699d4c572ecd3523c13697f1d
mrwarwick <joseph.glynn@warwick.ac.uk>
2025-02-23 08:23:12 +0000

    Revert "no working"
    This reverts commit 98f2ba7df29d5c4165cd2bcd7caff7b9a24a71aa.

--- drink_less/lib/main.dart
+++ drink_less/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:drink_less/pages/start_page.dart';
-import 'package:drink_less/pages/tts_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: MyHomePage(),
+      home: StartTestPage(),
     );
   }
 }
