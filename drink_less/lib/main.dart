c1683b8ad8f430e1d025c329c412f37c492a98c6
Joseph Glynn <josephnglynn@hotmail.co.uk>
2025-02-23 10:57:22 +0000

    move to start test pageg

--- drink_less/lib/main.dart
+++ drink_less/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: ResultsPage(),
+      home: StartTestPage(),
     );
   }
 }
