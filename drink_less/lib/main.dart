8ebcb44df37790b86db8051945794ea9ecb81b95
Joseph Glynn <josephnglynn@hotmail.co.uk>
2025-02-23 10:43:04 +0000

    start on start test page

--- drink_less/lib/main.dart
+++ drink_less/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:drink_less/pages/start_page.dart';
 import 'package:drink_less/results/results.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: ResultsPage(),
+      home: StartTestPage(),
     );
   }
 }
