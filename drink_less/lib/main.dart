b8a4b7b60ddddc3626fa6a44caa049e57b0de000
JonathanLu2005 <jonathanlu2005@yahoo.com>
2025-02-23 04:52:12 +0000

    refinements to results page

--- drink_less/lib/main.dart
+++ drink_less/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: Scaffold(
-        body: StartTestPage(),
+        body: ResultsPage(),
 
       ),
     );
